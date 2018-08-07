e327f2e4e3290f451f16c6875764d541cbba24ae
chendan <603446367@qq.com>
2018-08-07 17:26:42 +0800

    start

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <mmheader></mmheader>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="main">
+      container
+      <!-- <router-view/> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import mmheader from "./components/header/Header.vue"
+export default {
+  name: 'App',
+  components:{
+    mmheader
+  },
+  mounted(){
+    this.$axios('/api/seller').then(res=>{
+      console.log(res);
+    })
+  }
+}
+</script>
+
+<style>
+#app .tab{
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+}
+.tab-item{
+  flex: 1;
+  text-align: center;
+}
+</style>
