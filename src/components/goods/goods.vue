ca7797b3bc012c11ae287ae87714cf9c136d6d0d
chendan <603446367@qq.com>
2018-09-10 17:25:59 +0800

    商品展示

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,20 +1,46 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="(item,index) in goods" :key="index" class="menu-item">
+        <li v-for="(item,index) in goods" :key="index" class="menu-item" :class="{'current':currentIndex==index}" @click="selectMenu(index,$event)">
           <span class="text border-1px">
             <bg-icon v-show="item.type>0" class="icon" width="12" height="12" flag="3" :type="item.type"></bg-icon>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper" ref="foodsWrapper">
+      <ul>
+        <li v-for="(item,index) in goods" :key="index" class="food-list food-list-hook">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="(food,index) in item.foods" :key="index" class="food-item border-1px">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon" alt="">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
-const ERROK=0;
+import BScroll from "better-scroll";
 import bgIcon from "@/components/bgIcon/bgIcon"
+const ERROK=0;
 export default {
   name:'goods',
   props:{
@@ -27,7 +53,21 @@ export default {
   },
   data(){
     return {
-      goods:[]
+      goods:[],
+      listHeight:[],
+      scrollY:0
+    }
+  },
+  computed:{
+    currentIndex(){
+      for(let i=0;i<this.listHeight.length;i++){
+        let height=this.listHeight[i];
+        let nextHeight=this.listHeight[i+1];
+        if(!nextHeight||(this.scrollY>=height&&this.scrollY<nextHeight)){
+          return i;
+        }
+      }
+      return 0;
     }
   },
   created(){
@@ -35,8 +75,44 @@ export default {
       res=res.data;
       if(res.code==ERROK){
         this.goods=res.data;
+        this.$nextTick(()=>{//拿到数据后DOM更新
+          this._initScroll();
+          this._calculateHeight();
+        })
       }
     })
+  },
+  methods:{
+    _initScroll(){
+      this.menuScroll=new BScroll(this.$refs.menuWrapper,{
+        click:true
+      });
+      this.foodScroll=new BScroll(this.$refs.foodsWrapper,{
+        probeType:3//检测实时滚动的坐标
+      });
+
+      this.foodScroll.on('scroll',(pos)=>{
+        this.scrollY=Math.abs(Math.round(pos.y));
+      })
+    },
+    _calculateHeight(){
+      let foodList=this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let height=0;
+      this.listHeight.push(height);
+      for(let i=0;i<foodList.length;i++){
+        let item=foodList[i];
+        height+=item.clientHeight;
+        this.listHeight.push(height);
+      }
+    },
+    selectMenu(index,ev){
+      if(!ev._constructed){//自定义click事件时执行
+        return;
+      }
+      let foodList=this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let el=foodList[index];
+      this.foodScroll.scrollToElement(el,300);
+    }
   }
 }
 </script>
@@ -59,6 +135,14 @@ export default {
       height 54px
       line-height 14px
       padding 0 12px
+      &.current
+        position relative
+        z-index 10
+        margin-top -1px
+        background #fff
+        font-weight 700
+        .text
+          border-none()
       .icon
         margin-right 2px
       .text
@@ -69,5 +153,53 @@ export default {
         border-1px(rgba(7,17,27,.1))
   .foods-wrapper
     flex 1
+    .title
+      padding-left 14px
+      height 26px
+      line-height 26px
+      border-left 2px solid #d9dde1
+      font-size 12px
+      color rgb(147,153,159)
+      background #f3f7f5
+    .food-item
+      display flex
+      margin 18px
+      padding-bottom 18px
+      border-1px(rgba(7,17,27,.1))
+      &:last-child
+        border-none()
+        margin-bottom 0
+      .icon
+        flex 0 0 57px
+        margin-right 10px
+      .content
+        flex 1
+        .name
+          margin 2px 0 8px 0
+          height 14px
+          line-height 14px
+          font-size 14px
+          color rgb(7,17,27)
+        .desc,.extra
+          line-height 10px
+          font-size 10px
+          color rgb(147,153,159)
+        .desc
+          margin-bottom 8px
+          line-height 12px
+        .extra
+          .count
+            margin-right 12px
+        .price
+          font-weight 700
+          line-height 24px
+          .now
+            margin-right 8px
+            font-size 14px
+            color rgb(240,20,20)
+          .old
+            text-decoration line-through
+            font-size 10px
+            color rgb(147,153,159)
 </style>
 
