89aabeea6cb4ba064162e3cfe8cc1e02cab6ea5e
chendan <603446367@qq.com>
2018-10-09 09:56:47 +0800

    going

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="ratingselect">
     <div class="rating-type border-1px">
-      <span>{{desc.all}}</span>
-      <span>{{desc.positive}}</span>
-      <span>{{desc.negative}}</span>
+      <span class="block positive" :class="{'active':selectType===2}">{{desc.all}}
+        <span class="count">47</span>
+      </span>
+      <span class="block positive" :class="{'active':selectType===0}">{{desc.positive}}
+        <span class="count">50</span>
+      </span>
+      <span class="block negative" :class="{'active':selectType===1}">{{desc.negative}}
+        <span class="count">20</span>
+      </span>
     </div>
-    <div class="seitch">
+    <div class="switch" :class="{'on':onlyContent}">
       <i class="icon-check_circle"></i>
       <span class="text">只看有内容的评价</span>
     </div>
@@ -59,4 +65,44 @@ export default {
     padding 18px 0
     margin 0 18px
     border-1px(rgba(7,17,27,.1))
+    font-size 0
+    .block
+      display inline-block
+      padding 8px 12px
+      border-radius 1px
+      margin-right 8px
+      line-height 16px
+      color rgb(77,85,93)
+      font-size 12px
+      &.active
+        color #ffffff
+      .count
+        font-size 8px
+        margin-left 2px
+      &.positive
+        background rgba(0,160,220,.2)
+        &.active
+          background rgb(0,160,220)
+      &.negative
+        background rgba(77,85,93,.2) 
+        &.active
+          background rgb(77,85,93)
+  .switch
+    padding 12px 18px
+    line-height 24px
+    font-size 0
+    border-bottom 1px solid rgba(7,17,27,.1)
+    color rgb(147,153,159)
+    &.on
+      .icon-check_circle
+        color #00c850
+    .icon-check_circle
+      display inline-block
+      vertical-align top
+      font-size 24px
+      margin-right 4px
+    .text
+      display inline-block
+      vertical-align top
+      font-size 12px
 </style>
