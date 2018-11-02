829b3fb097bf7fa35eb6a0ac547612c3ba66e33f
chendan <603446367@qq.com>
2018-11-02 11:31:06 +0800

    fix

--- src/common/js/util.js
+++ src/common/js/util.js
@@ -1,22 +1,14 @@
 /**
  * 解析url参数
  * @example ？id=2345&a:b
- * @return Object {id:12345,a:b}
+ * @return getParameterByName(id) 2345
  */
-export function urlParse(){
-    let url=window.location.href;
-    //let url="http://localhost:8080/#/goods?id=2345&a:b"
-    let obj={};
-    let reg=/[?&][^?&]+=[^?&]+/g; //[^?&]:匹配 非 ？号和&符号
-    let arr=url.match(reg);
-    //['?id=12345','&a=b']
-    if(arr){
-        arr.forEach(item=>{
-            let tempArr=item.substring(1).split('=');
-            let key=decodeURIComponent(tempArr[0]);
-            let val=decodeURIComponent(tempArr[1]);
-            obj[key]=val;
-        })
-    }
-    return obj;
+export function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
